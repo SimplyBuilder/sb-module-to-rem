e169f715c868911f89bc891e91b9a3100cfc3b90
JamilServices <jamilservicos@gmail.com>
2024-01-31 22:03:49 -0300

    feat(toRem): Add additional tests for edge cases
    Added additional tests to cover edge cases for the toRem function, including:
    - Returning 0 when the input is 0, '0', '0px', '0em', '0pt', or '0rem'
    - Returning the correct value in rem when the input is a number or string

--- test/main.js
+++ test/main.js
@@ -5,24 +5,55 @@ import {describe, it} from "node:test";
 import {equal} from "node:assert";
 
 describe('toRem Tests', () => {
-    it('return 0.25rem on toRem(\'4px\')', () => {
-        const result = toRem('4px');
+
+    it('return 0 on toRem(0)', () => {
+        const result = toRem(0);
+        equal(result, '0');
+    });
+    it('return 0 on toRem(\'0\')', () => {
+        const result = toRem('0');
+        equal(result, '0');
+    });
+    it('return 0 on toRem(\'0px\')', () => {
+        const result = toRem('0px');
+        equal(result, '0');
+    });
+    it('return 0 on toRem(\'0em\')', () => {
+        const result = toRem('0em');
+        equal(result, '0');
+    });
+    it('return 0 on toRem(\'0pt\')', () => {
+        const result = toRem('0pt');
+        equal(result, '0');
+    });
+    it('return 0 on toRem(\'0rem\')', () => {
+        const result = toRem('0rem');
+        equal(result, '0');
+    });
+
+    it('return 0.25rem on toRem(4)', () => {
+        const result = toRem(4);
         equal(result, '0.25rem');
     });
     it('return 0.25rem on toRem(\'4\')', () => {
         const result = toRem('4');
         equal(result, '0.25rem');
     });
-    it('return 0 on toRem(\'0px\')', () => {
-        const result = toRem('0px');
-        equal(result, '0');
+    it('return 0.25rem on toRem(\'4px\')', () => {
+        const result = toRem('4px');
+        equal(result, '0.25rem');
     });
-    it('return 0 on toRem(\'4pt\')', () => {
+    it('return 4rem on toRem(\'4em\')', () => {
+        const result = toRem('4em');
+        equal(result, '4rem');
+    });
+    it('return 0.3332rem on toRem(\'4pt\')', () => {
         const result = toRem('4pt');
-        equal(result, '0');
+        equal(result, '0.3332rem');
     });
     it('return 0.25rem on toRem(\'0.25rem\')', () => {
         const result = toRem('0.25rem');
         equal(result, '0.25rem');
     });
+
 })
